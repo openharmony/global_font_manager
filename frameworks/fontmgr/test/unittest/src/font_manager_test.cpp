64c9c301c0532702c92ed18604442b95361ca90b
@ran-zhao-yu <ranzhaoyu1@huawei.com>
2025-06-03 19:54:23 +0800

    修复tdd中途退出告警
    Signed-off-by: @ran-zhao-yu <ranzhaoyu1@huawei.com>

--- frameworks/fontmgr/test/unittest/src/font_manager_test.cpp
+++ frameworks/fontmgr/test/unittest/src/font_manager_test.cpp
@@ -376,6 +376,7 @@ HWTEST_F(FontManagerTest, FontManagerFuncTest012, TestSize.Level1)
     }
     EXPECT_EQ(ret, SUCCESS);
     fd = open(fontPath4.c_str(), O_RDONLY);
+    ASSERT_TRUE(FileUtils::CheckPathExist("/data/test/testRepeats/"));
     FileUtils::CopyFile(fd, fontPath5);
     if (fd > 0) {
         close(fd);
@@ -387,9 +388,6 @@ HWTEST_F(FontManagerTest, FontManagerFuncTest012, TestSize.Level1)
         close(fd);
     }
     EXPECT_EQ(ret, SUCCESS);
-    if (!FileUtils::CheckPathExist("/data/test/testRepeats/")) {
-        return;
-    }
     std::filesystem::path rPath("/data/test/testRepeats/");
     for (const auto &file : std::filesystem::directory_iterator(rPath)) {
         if (file.is_regular_file()) {
@@ -419,6 +417,7 @@ HWTEST_F(FontManagerTest, FontManagerFuncTest013, TestSize.Level1)
     }
     EXPECT_EQ(ret, SUCCESS);
     fd = open(fontPath3.c_str(), O_RDONLY);
+    ASSERT_TRUE(FileUtils::CheckPathExist("/data/test/testRepeats/"));
     FileUtils::CopyFile(fd, fontPath6);
     if (fd > 0) {
         close(fd);
@@ -430,9 +429,6 @@ HWTEST_F(FontManagerTest, FontManagerFuncTest013, TestSize.Level1)
         close(fd);
     }
     EXPECT_EQ(ret, SUCCESS);
-    if (!FileUtils::CheckPathExist("/data/test/testRepeats/")) {
-        return;
-    }
     std::filesystem::path rPath("/data/test/testRepeats/");
     for (const auto &file : std::filesystem::directory_iterator(rPath)) {
         if (file.is_regular_file()) {
